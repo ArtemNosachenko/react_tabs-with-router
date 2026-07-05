225f33a73d299a8aec2a910ac3edc80bfe85e554
Artem Nosachenko <nosachenko.art@icloud.com>
2026-07-05 21:12:58 +0200

    add task correction

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom';
+
+type Tab = {
+  id: string;
+  title: string;
+  content: string;
+};
+
+type Props = {
+  tabs: Tab[];
+  activeTabId?: string;
+};
+
+export const Tabs = ({ tabs, activeTabId }: Props) => {
+  const activeTab = tabs.find(tab => tab.id === activeTabId);
+
+  return (
+    <>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={tab.id === activeTabId ? 'is-active' : ''}
+            >
+              <Link to={`/tabs/${tab.id}`} data-cy="TabLink">
+                {tab.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab ? activeTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
